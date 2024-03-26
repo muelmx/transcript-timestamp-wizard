06ae55d46923117af3acc3b7ce8e98d858b62ca0
Max <github@wabnap.de>
2024-03-26 12:32:10 +0100

    fix: tests

--- src/domain/transformer.test.ts
+++ src/domain/transformer.test.ts
@@ -18,28 +18,3 @@ test('transformer correctly transforms input without remove speakers', () => {
 
   expect(result.fileContent.value).toEqual(TestData.outputWithSpeakers);
 });
-
-test('illegal file content throws', () => {
-  expect(() =>
-    InputFile.fromString('file.txt', 'illegal file content'),
-  ).to.toThrowError('illegal file format');
-});
-
-test('illegal file from string throws', () => {
-  expect(() =>
-    InputFile.fromString('name.txt', 'illegal content'),
-  ).to.toThrowError('invalid input file name.txt: illegal file format');
-});
-
-test('opening output data throws', () => {
-  expect(() =>
-    InputFile.fromString('name.txt', TestData.outputWithoutSpeakers),
-  ).to.toThrowError('invalid input file name.txt: illegal file format');
-});
-
-test('output file name is extended', () => {
-  const result = Transformer.forInputFile(
-    InputFile.fromString('test.txt', TestData.input),
-  ).transform({ removeSpeakers: false });
-  expect(result.fileName.value).toBe('test_transformed.txt');
-});
